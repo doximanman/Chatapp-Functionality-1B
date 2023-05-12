f8835f8d9fc28f925f4d554b3f3dfd6316faa676
doximanman <kalo39570@gmail.com>
2023-05-12 08:55:12 +0300

    small behavioural fixes

--- src/Chat/ChatList.js
+++ src/Chat/ChatList.js
@@ -1,13 +1,16 @@
 import ChatPreview from "./ChatPreview";
 
+let selected = false;
+
 function ChatList({chats, user, setSelectedUser}) {
 
     function changeSelection(contact) {
         let allow = false;
-        if (window.innerWidth < 632) {
+        if (window.innerWidth <= 632) {
             document.getElementById('chat').classList.add('fullscreen');
             document.getElementById('chat-list').classList.add('hidden');
             allow = true;
+            selected = true;
         }
         if (allow || !contact.classes.includes('selected-preview')) {
             const selectedUser = chats.filter((contact) => {
@@ -19,26 +22,17 @@ function ChatList({chats, user, setSelectedUser}) {
         }
     }
 
-    /*function changeSelection(contact) {
-        if (window.innerWidth < 632) {
-            document.getElementById('chat').classList.add('fullscreen');
-            document.getElementById('chat-list').classList.add('hidden');
-            const selectedUser = chats.filter((contact) => {
-                return contact.classes.includes("selected-preview");
-            })[0];
-            selectedUser.classes.replace("selected-preview","");
-        }
-        if (!contact.classes.includes('selected-preview')) {
-            const selectedUsers = chats.filter((contact) => {
-                return contact.classes.includes("selected-preview");
-            });
-            if(selectedUsers.length>0){
-                selectedUsers[0].classes = selectedUsers[0].classes.replace("selected-preview", "");
+    window.addEventListener('resize', function () {
+        if (selected) {
+            if (window.innerWidth < 632) {
+                document.getElementById('chat').classList.add('fullscreen');
+                document.getElementById('chat-list').classList.add('hidden');
+            } else {
+                document.getElementById('chat').classList.remove('fullscreen');
+                document.getElementById('chat-list').classList.remove('hidden');
             }
-            contact.classes += " selected-preview";
-            setSelectedUser({...contact});
         }
-    }*/
+    })
 
     const chatList = chats.map((contact, key) => {
         return <ChatPreview contact={contact} changeSelection={changeSelection} messages={user.messages} key={key}/>
