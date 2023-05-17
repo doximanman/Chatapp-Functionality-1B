b0c2d1420b1941b2d0e9990371b6835359508744
EinatNoyman <110399343+EinatNoyman@users.noreply.github.com>
2023-05-17 15:58:23 +0300

    Fix block chat bug

--- src/Chat/Chat.js
+++ src/Chat/Chat.js
@@ -2,7 +2,7 @@ import "./Chat.css"
 import mainPFP from "../Pictures/user3-icon.jpg";
 import Profile from "./Profile";
 import ChatTitle from "./ChatTitle";
-import {useState} from "react";
+import { useState, useEffect } from "react";
 import MessageList from "./MessageList";
 import ChatList from "./ChatList";
 import contacts from "./Contacts"
@@ -10,25 +10,31 @@ import MessageSender from "./MessageSender";
 
 function Chat() {
 
-    // whenever a contact is added, the page is re-rendered.
-    const [chats,setContacts]=useState(contacts);
+    const [chats, setContacts] = useState(contacts);
 
     const user = {
-        pfp: mainPFP,
-        name: "Alice Smithhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh"
+        pfp: JSON.parse(sessionStorage.getItem('currentUser'))['picture'],
+        name: JSON.parse(sessionStorage.getItem('currentUser'))['displayName']
     }
 
-    const[selectedUser,setSelectedUser]=useState(chats.filter((contact) => {
+    const [selectedUser, setSelectedUser] = useState(chats.filter((contact) => {
         return contact.classes.includes("selected-preview");
     })[0]);
+
+    const navigate = useNavigate();
+    useEffect(() => {
+        if (!JSON.parse(sessionStorage.getItem('currentUser'))['username']) {
+            navigate("/Login");
+        }
+    }, []);
 
     return (
         <>
             <div id="main">
-                <Profile user={user} setContacts={setContacts}/>
-                <ChatList chats={chats} user={selectedUser} setSelectedUser={setSelectedUser}/>
+                <Profile user={user} setContacts={setContacts} />
+                <ChatList chats={chats} user={selectedUser} setSelectedUser={setSelectedUser} />
                 <div id="chat">
-                    <ChatTitle user={selectedUser}/>
+                    <ChatTitle user={selectedUser} />
                     <MessageList user={selectedUser} />
                     <MessageSender contact={selectedUser} setSelectedUser={setSelectedUser} />
                 </div>
